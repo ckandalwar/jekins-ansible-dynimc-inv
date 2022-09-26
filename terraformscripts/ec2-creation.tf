841fdfa852a1e407aa44c89b8da7af2c4034f516
ckandalwar <110048996+ckandalwar@users.noreply.github.com>
2022-09-26 22:47:00 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,11 +4,11 @@ provider "aws"{
 }
 
 resource "aws_instance" "AWSEC2Instance"{
-     count   = 2
+     count   = 1
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsjulybatch2022"
+     key_name        = "devops"
      tags = {
         Name = "tomcatservers"
      }
